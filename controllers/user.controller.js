7d9722ed0610d192834dd3d38068fb657be79162
divyanshisingh08 <divyanshisingh2308@gmail.com>
2025-02-04 11:26:11 +0530

    login controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
 const express=require("express");
 const User = require("../models/user.model");
+const bcrypt=require("bcryptjs")
+
 
 
 const register=async(req,res)=>{
@@ -15,9 +17,58 @@ const register=async(req,res)=>{
        if(user){
         throw new Error("User already exists")
        }
+
+
+       const hashedPassword= await bcrypt.hash(password,10);
+
+       await User.save({
+        fullName,
+        email,
+        password:hashedPassword,
+        phoneNumber,
+        role,
+       })
        
     } catch (error) {
         res.status(400).send("Something went wrong "+ error.message)
+        
+    }
+
+    
+}
+
+
+const login=async(req,res)=>{
+    try {
+        const {email,password,role}=req.body;
+
+    const user=await User.findOne({email})
+
+    if(!user){
+        throw new Error("User Does not exist");
+    }
+
+    const isPasswordValid= await bcrypt.compare(password,user.password);
+
+    if(!isPasswordValid){
+        throw new Error("Invalid Credentials")
+    }
+
+    if(role != user.role){
+        throw new Error("Account Does not exist with current role");
+
+    }
+
+
+
+    const tokenData={
+        userId:user._id
+    };
+
+const token =await jwt.sign()
+
+    
+    } catch (error) {
         
     }
 }
